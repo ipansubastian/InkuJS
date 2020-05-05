c84eb1064b184452896069c9518761598e457796
zaidhdev <zaid.harisah.zh9@gmail.com>
2020-05-05 15:08:28 +0700

    Menambah template warna baru, merubah kode menjadi lebih clean :)

--- etc/blend.js
+++ etc/blend.js
@@ -6,9 +6,15 @@
  * Anda dapat menambahkan, memodifikasi, ataupun menghapus sesuai kebutuhan anda.
  * Style dari teks berurut dari {warna latar depan} -> {warna latar belakang} -> {atribut teks}.
  * Setiap sub style dipisahkan dengan tanda titik. Contoh: 'red.bold.italic'
+ * Untuk lebih jelas silakan lihat Dokumentasi :
  * 
  */
-const $Banana = 'yellow..bold',
-      $DancingCheese = 'yellow..blink',
-      $Error = 'red..italic',
-      $DimWine = 'magenta..blink'
+const 
+    $Success       = 'green..bold'          ,
+    $Error         = 'red..italic'          ,
+    $ULError       = 'red..italic.underline',
+    $DancingCheese = 'yellow..italic.blink' ,
+    $BlinkCheese   = 'yellow..blink'        ,
+    $DimWine       = 'magenta..blink'       ,
+    $Blink         = 'white..blink'         ,
+    $BBlink        = 'white..blink.bold'
