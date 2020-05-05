eefd40341715119ed6ba9fa313292100e1b0f3c8
Zaid Harisah <52810608+zyddv@users.noreply.github.com>
2020-05-06 06:48:57 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -8,6 +8,10 @@ if [ ! -d InkuJS ]; then
         git clone https://github.com/zyddv/InkuJS
 fi;
 
+if [ -f 'node_modules/inkujs' ]; then
+        rm -rf node_modules/inkujs
+fi;
+
 mv InkuJS node_modules/inkujs
 
 echo -e '\e[32mInstallasi berhasil!'              
